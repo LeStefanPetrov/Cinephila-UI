8c375f49738962ff570c03c428c6c51de5852d18
LeStefanPetrov <stefanpetrovgm@gmail.com>
2022-11-05 14:19:51 +0200

    change API urls

--- cinephila-ui/src/environments/environment.prod.ts
+++ cinephila-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://home.com:8000/api/',
+  apiUrl: 'http://localhost:5000/api/',
 };
--- cinephila-ui/src/environments/environment.ts
+++ cinephila-ui/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://home.com:8000/api/',
+  apiUrl: 'http://localhost:5000/api/',
 };
 
 /*
